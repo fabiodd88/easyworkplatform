17f198d433a92ca26616f35e4dc694a7dd82a322
Ferdie <Ferdie@172.19.39.179>
2016-12-07 18:30:10 +0100

    Completamento ArticleModelDs

--- src/it/unisa/studenti/easyworkplatform/model/ArticleModelDS.java
+++ src/it/unisa/studenti/easyworkplatform/model/ArticleModelDS.java
@@ -1,38 +1,174 @@
 package it.unisa.studenti.easyworkplatform.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class ArticleModelDS implements ModelInterface<Article>{
 
+	private static final String TABLE_NAME = "article";
+	private static DataSource ds;
+	private static Connection connection;
+	private static PreparedStatement preparedStatement;
+	
+	//Costruttore ausiliare prima dell'ideazione della Classe Astratta per la connessione a database con nomi diversi
+	public ArticleModelDS() {	}
+	
+	public ArticleModelDS(String nomeDb) {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			ds = (DataSource) envCtx.lookup("jdbc/"+nomeDb);
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
-	public void insert(Article entity) throws SQLException {
-		// TODO Auto-generated method stub
-		
+	public void insert(Article article) throws SQLException {
+		String insertSql = "INSERT INTO " + ArticleModelDS.TABLE_NAME
+				+ "(article, price, desciption, duration)" + "VALUES (?,?,?,?)";
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(insertSql);
+			preparedStatement.setString(1, article.getName());
+			preparedStatement.setDouble(2, article.getPrice());
+			preparedStatement.setString(3, article.getDescription());
+			preparedStatement.setDate(4, (java.sql.Date)article.getDuration());
+			preparedStatement.executeUpdate();
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
 	}
 
 	@Override
-	public void update(Article entity) throws SQLException {
-		// TODO Auto-generated method stub
+	public void update(Article article) throws SQLException {
+		String updateSql = "UPDATE " + ArticleModelDS.TABLE_NAME
+				+ " SET(article = ?, price = ?, desciption = ?, duration = ?)" 
+				+ " WHERE (idArticle == ?)";
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(updateSql);
+			preparedStatement.setString(1, article.getName());
+			preparedStatement.setDouble(2, article.getPrice());
+			preparedStatement.setString(3, article.getDescription());
+			preparedStatement.setDate(4, (java.sql.Date)article.getDuration());
+			preparedStatement.setInt(5, article.getIdArticle());
+			preparedStatement.executeUpdate();
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
 		
 	}
 
 	@Override
 	public void remove(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		
+		String removeSql = "DELETE " + ArticleModelDS.TABLE_NAME + " WHERE (idArticle == ?)";
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(removeSql);
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
 	}
 
 	@Override
 	public Article findByKey(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Article article = new Article();
+		String selectSql = "SELECT * FROM " + ArticleModelDS.TABLE_NAME + " WHERE (idArticle == ?)";
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSql);
+			preparedStatement.setInt(1, id);
+			ResultSet rs = preparedStatement.executeQuery(selectSql);
+			while (rs.next()) {
+				article.setIdArticle(id);
+				article.setName(rs.getString("name"));
+				article.setPrice(rs.getDouble("price"));
+				article.setDescription(rs.getString("description"));
+				article.setDuration(rs.getDate("duration"));
+			}
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return article;
 	}
 
 	@Override
 	public LinkedList<Article> findAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedList<Article> listArticle = new LinkedList<Article>();
+		String selectSql = "SELECT * FROM " + ArticleModelDS.TABLE_NAME;
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSql);
+			ResultSet rs = preparedStatement.executeQuery(selectSql);
+			while (rs.next()) {
+				Article article = new Article();
+				article.setIdArticle(rs.getInt("idArticle"));
+				article.setName(rs.getString("name"));
+				article.setPrice(rs.getDouble("price"));
+				article.setDescription(rs.getString("description"));
+				article.setDuration(rs.getDate("duration"));
+				listArticle.add(article);
+			}
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return listArticle;
 	}
 
 }
