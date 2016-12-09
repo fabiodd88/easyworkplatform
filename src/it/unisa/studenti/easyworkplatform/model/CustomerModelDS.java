10f753c1aa6770f539f20772cc8a8a813fad25bc
fabiodd88 <fabio.didomenico88@gmail.com>
2016-12-09 16:07:31 +0100

    Implementazione dei metodi insert & update

--- src/it/unisa/studenti/easyworkplatform/model/CustomerModelDS.java
+++ src/it/unisa/studenti/easyworkplatform/model/CustomerModelDS.java
@@ -1,26 +1,94 @@
 package it.unisa.studenti.easyworkplatform.model;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.LinkedList;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
 public class CustomerModelDS implements ModelInterface<Customer>{
 
-	@Override
-	public void insert(Customer entity) throws SQLException {
-		// TODO Auto-generated method stub
-		
+	private static final String TABLE_NAME = "customer";
+	private static DataSource ds;
+	private static Connection connection;
+	private static PreparedStatement preparedStatement;
+
+	public CustomerModelDS(){	}
+
+	public CustomerModelDS(String nomeDb) {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			ds = (DataSource) envCtx.lookup("jdbc/"+nomeDb);
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 	}
 
+
+	private void preparedStat(Customer customer){
+		try{
+			preparedStatement.setString(1, customer.getName());
+			preparedStatement.setString(2, customer.getSurename());
+			preparedStatement.setDate(3, (Date) customer.getBirthdate());
+			preparedStatement.setString(4, customer.getBirthplace());
+			preparedStatement.setString(5, customer.getAddress());
+			preparedStatement.setString(6, customer.getCity());
+			preparedStatement.setString(7, customer.getProvince());
+			preparedStatement.setInt(8, customer.getCap());
+			preparedStatement.setString(9, customer.getPhoneNumber());
+			preparedStatement.setInt(10, customer.getNewsletter());
+			preparedStatement.setString(11, customer.getEmail());
+			preparedStatement.executeUpdate();
+			connection.commit();
+		}
+		catch(SQLException e){	}
+		finally
+		{
+			try{
+				if (preparedStatement != null)  preparedStatement.close();
+				if (connection != null) 		connection.close();	
+			}
+			catch(SQLException e){	}
+		}
+
+	}
+
+
 	@Override
-	public void update(Customer entity) throws SQLException {
-		// TODO Auto-generated method stub
-		
+	public void insert(Customer customer) throws SQLException {
+
+		String insertSql = "INSERT INTO " + CustomerModelDS.TABLE_NAME
+				+ "(name, surename, birth_date, birth_place, address, city, province, cap, phone_number, newsletter, email)" + " VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+		connection = ds.getConnection();
+		preparedStatement = connection.prepareStatement(insertSql);
+		preparedStat(customer);	
+	}
+
+
+	@Override
+	public void update(Customer customer) throws SQLException {
+		String selectSql="UPDATE"+CustomerModelDS.TABLE_NAME+
+				"SET name=?,surename=?,birth_date=?,birth_place=?,address=?,city=?,province=?,"
+				+ "cap=?,phone_number=?,newsletter=?,email=?)"
+				+ "WHERE id=?";
+		connection = ds.getConnection();
+		preparedStatement = connection.prepareStatement(selectSql);
+		preparedStat(customer);
+
+
 	}
 
 	@Override
 	public void remove(int id) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
