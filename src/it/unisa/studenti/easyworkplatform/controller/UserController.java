f5e170816e670a43d9103304bba3b42efaf66e9b
Ferdie <Ferdie@DESKTOP-M5KIOFN>
2017-01-07 18:31:32 +0100

    Correzione Metodi

--- src/it/unisa/studenti/easyworkplatform/controller/UserController.java
+++ src/it/unisa/studenti/easyworkplatform/controller/UserController.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 import it.unisa.studenti.easyworkplatform.model.Account;
 import it.unisa.studenti.easyworkplatform.model.AccountModelDS;
